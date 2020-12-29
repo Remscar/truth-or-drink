866cd468b08c046df0712e17bdcd5fdc0bab798b
Remscar <remscar@gmail.com>
2020-12-28 20:51:42 -0800

    lobby started

--- src/components/game/lobby.tsx
+++ src/components/game/lobby.tsx
@@ -1,6 +1,7 @@
 import { Grid, Typography } from "@material-ui/core";
 import React from "react";
 import { useStartedGameState } from "../../hooks/useGameState";
+import { StyledButton } from "../button";
 
 export const GameLobby: React.FC = (props) => {
   const gameState = useStartedGameState();
@@ -18,9 +19,24 @@ export const GameLobby: React.FC = (props) => {
           <Typography variant="h3" align="center">
             Lobby
           </Typography>
+        </Grid>
+        <Grid item>
           <Typography variant="h4" align="center">
             {gameState.state.gameCode}
           </Typography>
+        </Grid>
+        <Grid item container direction="column" xs={12}>
+          <Grid item>
+            <Typography align="center">Names</Typography>
+          </Grid>
+        </Grid>
+        <Grid item container direction="row" justify="space-around">
+          <Grid item>
+            <StyledButton color="red">Start Game</StyledButton>
+          </Grid>
+          <Grid item>
+            <StyledButton color="gray">Leave</StyledButton>
+          </Grid>
         </Grid>
       </Grid>
     </React.Fragment>
